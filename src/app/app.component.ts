03174c89558081ae1f55c37af4e31831ebed5d08
Nephi Szvoboda <nephi.szvoboda@gmail.com>
2020-08-24 14:49:20 +0200

    adding working exmaple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { hyphenate } from 'hyphen/en';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'kendo-pdf-hyphenation';
+export class AppComponent implements OnInit {
+  public title = 'kendo-pdf-hyphenation';
+
+  public romeoAndJulietPrologue = `
+  Some of the Longest Words in English:
+  Pneumonoultramicroscopicsilicovolcanoconiosis,
+  Pseudopseudohypoparathyroidism,
+  Floccinaucinihilipilification,
+  Antidisestablishmentarianism,
+  supercalifragilisticexpialidocious,
+  Incomprehensibilities`;
+
+  public romeoAndJulietPrologueHypenated: string;
+
+  public ngOnInit(): void {
+    hyphenate(this.romeoAndJulietPrologue).then(
+      (result) => (this.romeoAndJulietPrologueHypenated = result)
+    );
+  }
 }
